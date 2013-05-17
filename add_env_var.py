334006f8976eaa93236fc0490d2f6844cd638643
BenTobias <benedictljj@gmail.com>
2013-05-17 21:42:38 +0800

    Moved functions to main().

--- add_env_var.py
+++ add_env_var.py
@@ -1,20 +1,25 @@
 import os
 import sys
+import environmentvariables_pb2
 
 # Hardcoded values
 # Must change using some sort of data representation (protobuf? json?)
 
 # Main procedure
-is_valid = False
+def main():
+    is_valid = False
 
-while not is_valid:
-    chromedriver_path = raw_input("Enter path to chromedriver: ")
-    url_input = raw_input("Enter URL to observe: ")
+    while not is_valid:
+        chromedriver_path = raw_input("Enter path to chromedriver: ")
+        url_input = raw_input("Enter URL to observe: ")
 
-    if chromedriver_path and url_input:
-        is_valid = True
+        if chromedriver_path and url_input:
+            is_valid = True
 
-f = open("EnvFile", "wb")
-f.write(chromedriver_path + "\n")
-f.write(url_input)
-f.close()
+    f = open("EnvFile", "wb")
+    f.write(chromedriver_path + "\n")
+    f.write(url_input)
+    f.close()
+
+if __name__ == '__main__':
+    main()
